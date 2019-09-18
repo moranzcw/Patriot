889938de38c69a9dc081f778c44372b209af5d51
moranzcw <moranzcw@gmail.com>
2019-09-18 20:16:02 +0800

    .

--- server/install.sh
+++ server/install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+mkdir ss
+cd ss
+
 apt update
-apt install python-pip
-apt install python-setuptools
+apt install -y python-pip
+apt install -y python-setuptools
+apt install -y shadowsocks
 ssserver -p 11170 -k 112358 -m aes-256-cfb --user nobody -d start
 
-./server_linux_amd64 -t "127.0.0.1:11170" -l ":4000" -mode fast3 -nocomp -sockbuf 16777217 -dscp 46
+wget https://github.com/moranzcw/Patriot/raw/master/server/server_linux_amd64
+chmod +777 ./server_linux_amd64
+nohup ./server_linux_amd64 -t "127.0.0.1:11170" -l ":4000" -mode fast3 -nocomp -sockbuf 16777217 -dscp 46 &
